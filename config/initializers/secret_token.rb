10a3534f85dda54f7c1a9791ceb5a0a709928bd4
Andrew Kimn <akimn89@gmail.com>
2013-11-17 00:03:24 -0800

    First add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unacracy::Application.config.secret_key_base = '81875ad3d6374850e627d67b070bcca54e81df6612a9b4c89c8397efec055fcbb79579e8a0a1c7c1dee8abb2b2a1018116611d6ab979fd322803f435104a1ef2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Unacracy::Application.config.session_store :cookie_store, key: '_unacracy_session'
