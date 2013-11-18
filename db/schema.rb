63951e241da129e73ebd305ff43b7835f8094ef8
Andrew Kimn <akimn89@gmail.com>
2013-11-18 14:26:39 -0800

    added a whole bunch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118200141) do
+ActiveRecord::Schema.define(version: 20131118214821) do
 
   create_table "pins", force: true do |t|
     t.string   "Church_Name"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20131118200141) do
     t.string   "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
