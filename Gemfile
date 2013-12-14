735fd78743725a1205e1bfca181f68fbce30ca67
Andrew Kimn <akimn89@gmail.com>
2013-12-13 21:10:19 -0800

    stuff

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :development, :test do
 end
 
 group :production do
+	gem 'sqlite3'
 	gem 'pg'
 	gem 'rails_12factor'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_token
+
 	has_many :pins
 end
