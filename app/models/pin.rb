643cfee3c4997d3d1df4b8c0188539b33a047119
Andrew Kimn <akimn89@gmail.com>
2013-12-15 22:17:32 -0800

    commit

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,8 +5,8 @@ class Pin < ActiveRecord::Base
 		
 	   if search
 	   #	@items = Item.where('name LIKE ? OR description LIKE ? OR category LIKE ?', key, key, key).order(:name)
-      	where('comment LIKE ? OR church_name LIKE ? OR pastor_or_worship_leader_name LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
-      	#where('comment LIKE ? OR pastor_or_worship_leader_name LIKE ?', "%#{search}%", "%#{search}%")
+      	#where('comment LIKE ? OR church_name LIKE ? OR pastor_or_worship_leader_name LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
+      	where('comment LIKE ? OR Pastor_or_Worship_Leader_Name LIKE ?', "%#{search}%", "%#{search}%")
       	
       	 #if search
 	   #where('comment LIKE ?', "%#{search}%")
