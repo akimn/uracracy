f17955647055f3241a8cb04c9eb2376e64179f9c
Andrew Kimn <akimn89@gmail.com>
2013-12-22 14:13:35 -0800

    stuff

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -17,7 +17,7 @@ class Pin < ActiveRecord::Base
 	def self.search(search)
 		
 	  if search	
-      where('comment ILIKE ? OR "Church_Name" ILIKE ? OR "Pastor_or_Worship_Leader_Name" ILIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
+      where('comment ILIKE ? OR "church_name" ILIKE ? OR "pastor_or_worship_leader_name" ILIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
 	  else
 	    scoped
 	  end
