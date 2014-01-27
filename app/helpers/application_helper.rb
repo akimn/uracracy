4dffe56daadd3883531cef73c3abd85f21e0bf61
Andrew Kimn <akimn89@gmail.com>
2014-01-26 21:18:17 -0800

    stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,3 +6,6 @@ module ApplicationHelper
 		link_to title, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
 	end
 end
+
+<%= form_for(resource, :as => resource_name, :url => registration_path(resource_name)) do |f| %>
+	  <%= devise_error_messages! %>
